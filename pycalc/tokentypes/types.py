88786c671860eba40fae1b03e81b5336bea7d5b9
a very fake floordiv <real.floordiv@gmail.com>
2022-06-16 00:59:59 +0200

    new enums & operators
    added a lot of new enums, operators, even new priorities. Seems like this won't be edited anymore

--- pycalc/tokentypes/types.py
+++ pycalc/tokentypes/types.py
@@ -8,44 +8,98 @@ ALLOWED_LITERALS = ascii_letters + "_"
 class LexemeType(enum.IntEnum):
     UNKNOWN = 0
     NUMBER = 1
-    LITERAL = 2
+    HEXNUMBER = 2
+    LITERAL = 3
+    OPERATOR = 4
+    LBRACE = 5
+    RBRACE = 6
+    DOT = 7
+    COMMA = 8
+
+
+class TokenKind(enum.IntEnum):
+    NUMBER = 0
+    LITERAL = 1
+    BRACE_EXPR = 2
     OPERATOR = 3
-    LBRACE = 4
-    RBRACE = 5
-    DOT = 6
-    COMMA = 7
+    UNARY_OPERATOR = 4
+    FUNC = 5
 
 
 class TokenType(enum.IntEnum):
-    NUMBER = 1
-    LITERAL = 2
-    BRACE_EXPR = 4
-    FUNC = 8
-    OP_ADD = 16
-    OP_SUB = 32
-    OP_DIV = 64
-    OP_MUL = 128
-    COMMA = 256
+    OP_EQ = 0
+    OP_EQEQ = 1
+    OP_NOTEQ = 2
+    OP_ADD = 3
+    OP_SUB = 4
+    OP_DIV = 5
+    OP_MUL = 6
+    OP_POW = 7
+    OP_LSHIFT = 8
+    OP_RSHIFT = 9
+    OP_BITWISE_AND = 10
+    OP_BITWISE_OR = 11
+    OP_BITWISE_XOR = 12
+    OP_MOD = 13
+    OP_FLOORDIV = 14
+    OP_SEMICOLON = 15
+    UN_POS = 16
+    UN_NEG = 17
+    COMMA = 18
+    FUNCCALL = 19
+    FUNCDECL = 20
+    VARDECL = 21
 
 
-OPERATOR = TokenType.OP_ADD | TokenType.OP_SUB | TokenType.OP_DIV | TokenType.OP_MUL
 OPERATORS_TABLE = {
     "+": TokenType.OP_ADD,
     "-": TokenType.OP_SUB,
     "/": TokenType.OP_DIV,
+    "//": TokenType.OP_FLOORDIV,
     "*": TokenType.OP_MUL,
+    "**": TokenType.OP_POW,
+    "%": TokenType.OP_MOD,
+    "<<": TokenType.OP_LSHIFT,
+    ">>": TokenType.OP_RSHIFT,
+    "&": TokenType.OP_BITWISE_AND,
+    "|": TokenType.OP_BITWISE_OR,
+    "^": TokenType.OP_BITWISE_XOR,
+
+    "==": TokenType.OP_EQEQ,
+    "!=": TokenType.OP_NOTEQ,
+
+    ";": TokenType.OP_SEMICOLON,
+    "=": TokenType.OP_EQ
 }
 
 
 class Priorities(enum.IntEnum):
+    NONE = 0
     MINIMAL = 1
     MEDIUM = 2
     HIGH = 3
+    MAXIMAL = 4
 
 
 PRIORITIES_TABLE = {
-    TokenType.OP_ADD: Priorities.MINIMAL,
-    TokenType.OP_SUB: Priorities.MINIMAL,
-    TokenType.OP_DIV: Priorities.MEDIUM,
-    TokenType.OP_MUL: Priorities.MEDIUM,
+    TokenType.OP_ADD:      Priorities.MINIMAL,
+    TokenType.OP_SUB:      Priorities.MINIMAL,
+    TokenType.OP_DIV:      Priorities.MEDIUM,
+    TokenType.OP_FLOORDIV: Priorities.MEDIUM,
+    TokenType.OP_MUL:      Priorities.MEDIUM,
+    TokenType.UN_POS:      Priorities.HIGH,
+    TokenType.UN_NEG:      Priorities.HIGH,
+    TokenType.OP_POW:      Priorities.MAXIMAL,
+
+    TokenType.OP_MOD:         Priorities.MEDIUM,
+    TokenType.OP_LSHIFT:      Priorities.MEDIUM,
+    TokenType.OP_RSHIFT:      Priorities.MEDIUM,
+    TokenType.OP_BITWISE_AND: Priorities.MEDIUM,
+    TokenType.OP_BITWISE_OR:  Priorities.MEDIUM,
+    TokenType.OP_BITWISE_XOR: Priorities.MEDIUM,
+
+    TokenType.OP_EQ:        Priorities.NONE,
+    TokenType.OP_EQEQ:      Priorities.NONE,
+    TokenType.OP_NOTEQ:     Priorities.NONE,
+    TokenType.OP_SEMICOLON: Priorities.NONE,
 }
