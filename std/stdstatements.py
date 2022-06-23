83b5083aa9ac38f16994ec10673887c8548e58c4
a very fake floordiv <real.floordiv@gmail.com>
2022-06-23 23:49:28 +0200

    creations
    stdstatements is a file that brings different statements (or I'd like to say functions that replace statements). For example, cond()

---
+++ std/stdstatements.py
@@ -0,0 +1,14 @@
+from typing import Callable
+
+from pycalc.tokentypes.types import Number
+
+
+def cond(condition: Callable, if_cb: Callable, else_cb: Callable) -> Number:
+    return if_cb() if condition() else else_cb()
+
+
+def if_(condition: Callable, cb: Callable):
+    if condition():
+        cb()
+
+    return 0
