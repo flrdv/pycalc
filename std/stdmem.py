58ec2b9aae240bf389f4701e5786feebecfa4db6
a very fake floordiv <real.floordiv@gmail.com>
2022-06-24 00:10:54 +0200

    deletions
    after I separated floats and integers, I finally can move all the checks "is provided number an integer" (sometimes I really had integer, so it was AttributeError: int has no attribute called is_integer()) to a trashcan

--- std/stdmem.py
+++ std/stdmem.py
@@ -1,29 +1,18 @@
 from typing import List
 
-from pycalc.tokentypes.types import Number
+
+def mem_alloc(size: int) -> List[int]:
+    return [0] * size
 
 
-def mem_alloc(size: Number) -> List[int]:
-    if not size.is_integer():
-        raise TypeError("malloc() takes only integers")
-
-    return [0] * int(size)
-
-
-def mem_get(mem: List[int], offset: Number) -> int:
-    if not offset.is_integer():
-        raise TypeError("get() takes only integers")
-
+def mem_get(mem: List[int], offset: int) -> int:
     if 0 > offset >= len(mem):
         return -1
 
     return mem[int(offset)]
 
 
-def mem_set(mem: List[int], offset: Number, value: Number) -> int:
-    if not offset.is_integer() or not value.is_integer():
-        raise TypeError("set() takes only integers")
-
+def mem_set(mem: List[int], offset: int, value: int) -> int:
     if 0 > offset >= len(mem) or 0 > value > 255:
         return -1
 
