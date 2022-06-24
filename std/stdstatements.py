f11011432a14ff4bb13a6c159958fd9d4f55fa8d
a very fake floordiv <real.floordiv@gmail.com>
2022-06-24 18:57:59 +0200

    additions
    added returned value type-hint for `if_` function

--- std/stdstatements.py
+++ std/stdstatements.py
@@ -7,7 +7,7 @@ def cond(condition: Callable, if_cb: Callable, else_cb: Callable) -> Number:
     return if_cb() if condition() else else_cb()
 
 
-def if_(condition: Callable, cb: Callable):
+def if_(condition: Callable, cb: Callable) -> int:
     if condition():
         cb()
 
